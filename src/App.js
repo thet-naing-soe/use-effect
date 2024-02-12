91fc19e87ebf990d1b8a06bd24eee677170cbff3
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-02-12 09:06:01 +0630

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
+  const [toggle, setToggle] = React.useState(false);
+  function clickHandler() {
+    setToggle(!toggle);
+  }
+  React.useEffect(() => {
+    document.title = toggle
+      ? "Welcome to Little Lemon"
+      : "Using the useEffect Hook"
+  }, [toggle]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Using the useEffect Hook</h1>
+      <button onClick={clickHandler}>Toggle message</button>
+      {toggle && <h2>Welcome to Little Lemon</h2>}
     </div>
   );
 }
-
-export default App;
